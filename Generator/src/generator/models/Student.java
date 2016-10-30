c1c2787af48dc1c12a4fe7af5de0b4539d16c4bf
ShaggyNipple <dawid.kaszubski66@gmail.com>
2016-10-30 18:10:49 +0100

    Added Student

---
+++ Generator/src/generator/models/Student.java
@@ -0,0 +1,40 @@
+package generator.models;
+
+import generator.PersonalDataGenerator;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Dawid on 2016-10-30.
+ */
+public class Student extends Person {
+    private SchoolClass schoolClass;
+    private String address;
+
+    public Student(PersonalDataGenerator pdGenerator, SchoolClass schoolClass) {
+        super(16+ LocalDate.now().getYear()-schoolClass.getStartYear(),16+ LocalDate.now().getYear()-schoolClass.getStartYear(),pdGenerator);
+        this.schoolClass=schoolClass;
+        address= pdGenerator.getStreet() + " " + pdGenerator.getHouseNumber() + " "+ pdGenerator.getCity();
+    }
+
+    public SchoolClass getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(SchoolClass schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+
+    public String getAdress() {
+        return address;
+    }
+
+    public void setAdress(String adress) {
+        this.address = adress;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " "+ schoolClass.toString() + " " + address;
+    }
+}
